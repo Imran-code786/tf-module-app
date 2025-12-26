fa48e5e02464d84e6b81a64fdf350e51adef88a5
Imran <imrankhanmohammed130@gmail.com>
2025-12-26 14:30:58 -0600

    tf modile

--- main.tf
+++ main.tf
@@ -1,8 +1,24 @@
-resource "null_resource" "test" {
-    triggers = {
-        xyz = timestamp()
-    }
-    provisioner "local-exec"{
-        command = "echo Hello world -Env - ${var.env}"
-    }
-}
+resource "aws_iam_policy" "policy" {
+  name  = "${var.component}-${var.env}-ssm-pm-policy"
+  path = "/"
+  description = "${var.component}-${var.env}-ssm-pm-policy"
+
+  policy = jsonencode( {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ssm:GetParameterHistory",
+                "ssm:GetParametersByPath",
+                "ssm:GetParameters",
+                "ssm:GetParameter"
+            ],
+            "Resource": "arn:aws:ssm:us-east-1:950538586636:parameter/roboshop.${var.env}.${var.component}.*"
+        }
+    ]
+})
+  }
+
+# I AM ROLE
